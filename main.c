94bc7ebf60058d848e329567713028f1d5fd5441
HLG <hlg@etale.com.ar>
2017-02-14 17:14:06 -0300

    inital commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "funcs.h"
+
+int
+main(int argc, char **argv)
+{
+    test();
+    a();
+    test();
+    a();
+    return 0;
+}
---
+++ np.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "funcs.h"
+void a(void)
+{
+    printf("hahaahahahah\n");
+}
+void test(void)
+{
+
+    printf("this is function test\n");
+}
